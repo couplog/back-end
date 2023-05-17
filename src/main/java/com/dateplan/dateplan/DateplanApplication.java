dcbe30a393c5fb9d93f410ca10313701a6f88e34
guswls159357 <guswls159357@naver.com>
2023-05-17 20:17:37 +0900

    init

---
+++ src/main/java/com/dateplan/dateplan/DateplanApplication.java
@@ -0,0 +1,13 @@
+package com.dateplan.dateplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DateplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DateplanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dateplan/dateplan/DateplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dateplan.dateplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DateplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

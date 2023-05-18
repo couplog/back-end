54af314289d0c2ddc832329a973ad5af03ba09f8
kariskan <zkfzpf56@naver.com>
2023-05-18 20:55:52 +0900

    [style - #3] 코드 리포매팅

--- src/main/java/com/dateplan/dateplan/global/config/jasypt/JasyptConfig.java
+++ src/main/java/com/dateplan/dateplan/global/config/jasypt/JasyptConfig.java
@@ -1,5 +1,6 @@
 package com.dateplan.dateplan.global.config.jasypt;
 
+import lombok.RequiredArgsConstructor;
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
@@ -7,29 +8,27 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import lombok.RequiredArgsConstructor;
-
 @RequiredArgsConstructor
 @Configuration
 public class JasyptConfig {
 
-	@Value("${jasypt.password}")
-	private String encryptKey;
+		@Value("${jasypt.password}")
+		private String encryptKey;
 
-	@Bean("jasyptStringEncryptor")
-	public StringEncryptor stringEncryptor(){
-		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
-		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-		config.setPassword(encryptKey);
-		config.setAlgorithm("PBEWithMD5AndDES");
-		config.setKeyObtentionIterations("1000");
-		config.setPoolSize("1");
+		@Bean("jasyptStringEncryptor")
+		public StringEncryptor stringEncryptor() {
+				PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+				SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+				config.setPassword(encryptKey);
+				config.setAlgorithm("PBEWithMD5AndDES");
+				config.setKeyObtentionIterations("1000");
+				config.setPoolSize("1");
 
-		config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
-		config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator");
-		config.setStringOutputType("base64");
-		encryptor.setConfig(config);
-		return encryptor;
-	}
+				config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+				config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator");
+				config.setStringOutputType("base64");
+				encryptor.setConfig(config);
+				return encryptor;
+		}
 }
 

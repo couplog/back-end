3693540760c37942c8816b67d479c5cde7bc260d
kariskan <zkfzpf56@naver.com>
2023-05-19 15:00:15 +0900

    [style - #3] 코드 리포맷
    - indent 2 -> 4

--- src/main/java/com/dateplan/dateplan/global/config/jasypt/JasyptConfig.java
+++ src/main/java/com/dateplan/dateplan/global/config/jasypt/JasyptConfig.java
@@ -12,23 +12,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class JasyptConfig {
 
-		@Value("${jasypt.password}")
-		private String encryptKey;
+	@Value("${jasypt.password}")
+	private String encryptKey;
 
-		@Bean("jasyptStringEncryptor")
-		public StringEncryptor stringEncryptor() {
-				PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
-				SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-				config.setPassword(encryptKey);
-				config.setAlgorithm("PBEWithMD5AndDES");
-				config.setKeyObtentionIterations("1000");
-				config.setPoolSize("1");
+	@Bean("jasyptStringEncryptor")
+	public StringEncryptor stringEncryptor() {
+		PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+		SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+		config.setPassword(encryptKey);
+		config.setAlgorithm("PBEWithMD5AndDES");
+		config.setKeyObtentionIterations("1000");
+		config.setPoolSize("1");
 
-				config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
-				config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator");
-				config.setStringOutputType("base64");
-				encryptor.setConfig(config);
-				return encryptor;
-		}
+		config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+		config.setIvGeneratorClassName("org.jasypt.iv.NoIvGenerator");
+		config.setStringOutputType("base64");
+		encryptor.setConfig(config);
+		return encryptor;
+	}
 }
 

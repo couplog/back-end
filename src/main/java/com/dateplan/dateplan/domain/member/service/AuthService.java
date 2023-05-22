c8a68e86fb7399f4d937d8b19058be7ae5c656d2
guswls159357 <guswls159357@naver.com>
2023-05-22 23:38:24 +0900

    [feat - #7] 전화번호 인증 메소드 작성
    추가로 새로운 인증 코드를 발급받는다면 기존에 있던 데이터를 모두 삭제 후 다시 저장하도록 수정

--- src/main/java/com/dateplan/dateplan/domain/member/service/AuthService.java
+++ src/main/java/com/dateplan/dateplan/domain/member/service/AuthService.java
@@ -1,8 +1,11 @@
 package com.dateplan.dateplan.domain.member.service;
 
+import com.dateplan.dateplan.domain.member.dto.PhoneAuthCodeServiceRequest;
 import com.dateplan.dateplan.domain.member.dto.PhoneServiceRequest;
 import com.dateplan.dateplan.domain.sms.service.SmsService;
+import com.dateplan.dateplan.global.exception.InvalidPhoneAuthCodeException;
 import com.dateplan.dateplan.global.util.RandomCodeGenerator;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.ListOperations;
@@ -25,9 +28,23 @@ public class AuthService {
 		String phone = request.getPhone();
 
 		memberReadService.throwIfPhoneExists(phone);
-		smsService.sendSmsForPhoneAuthentication(phone, code);
+		//smsService.sendSmsForPhoneAuthentication(phone, code);
 
 		saveAuthCodeInRedis(phone, code);
+	}
+
+	public void authenticateAuthCode(PhoneAuthCodeServiceRequest request) {
+
+		ListOperations<String, String> opsForList = redisTemplate.opsForList();
+
+		String phone = request.getPhone();
+		String key = AUTH_KEY_PREFIX + phone;
+		String savedCode = opsForList.index(key, 0);
+
+		validateCode(savedCode, request.getCode());
+
+		opsForList.rightPop(key);
+		opsForList.rightPush(key, Boolean.TRUE.toString());
 	}
 
 	private void saveAuthCodeInRedis(String phone, int code) {
@@ -36,8 +53,17 @@ public class AuthService {
 
 		String key = AUTH_KEY_PREFIX + phone;
 
+		redisTemplate.delete(key);
+
 		opsForList.rightPush(key, String.valueOf(code));
 		opsForList.rightPush(key, String.valueOf(false));
-		redisTemplate.expire(key, 1, TimeUnit.MINUTES);
+		redisTemplate.expire(key, 2, TimeUnit.MINUTES);
+	}
+
+	private void validateCode(String code, String input) {
+
+		if (code == null || !Objects.equals(input, code)) {
+			throw new InvalidPhoneAuthCodeException(code);
+		}
 	}
 }

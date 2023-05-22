c0d8c42c09a3eaba73599afdb1f7f343be93e075
kariskan <zkfzpf56@naver.com>
2023-05-22 21:16:50 +0900

    [feat - #12] api/auth 패턴에 인터셉터가 동작하지 않도록 변경

--- src/main/java/com/dateplan/dateplan/global/config/web/WebConfig.java
+++ src/main/java/com/dateplan/dateplan/global/config/web/WebConfig.java
@@ -1,5 +1,6 @@
 package com.dateplan.dateplan.global.config.web;
 
+import com.dateplan.dateplan.global.auth.JwtProvider;
 import com.dateplan.dateplan.global.interceptor.AuthInterceptor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
@@ -10,10 +11,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-	private final AuthInterceptor authInterceptor;
+	private final JwtProvider jwtProvider;
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(authInterceptor).addPathPatterns("/**");
+		registry
+			.addInterceptor(new AuthInterceptor(jwtProvider))
+			.addPathPatterns("/**")
+			.excludePathPatterns("/api/auth/**");
 	}
 }

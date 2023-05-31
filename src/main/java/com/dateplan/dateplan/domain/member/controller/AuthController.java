bc6e9de999e189e1ddd3a104eebf548160925d2c
kariskan <73534426+kariskan@users.noreply.github.com>
2023-05-31 23:24:10 +0900

    Feat/#19 login test (#21)
    * [test - #19] login test
    
    - login 성공
    - 올바르지 않은 패턴의 전화번호
    - 가입되지 않은 전화번호
    - 올바르지 않은 패스워드
    
    * [fix - #19] BEARER 상수 수정
    
    - "BEARER " -> "Bearer "
    
    * [feat - #19] - setHeader 방식 변경
    
    - 기존의 service layer에서 AuthToken을 set하던 것을 controller layer에서 set 하도록 변경
    
    * [test - #19] - refresh test
    
    - 기존 로그인 테스트 수정
    - refresh 성공
    - Authorization 헤더 존재하지 않을 때
    - 유효하지 않은 토큰 입력했을 때
    - 토큰으로 회원을 찾을 수 없을 때
    
    * [test - #19] - jwt provider test
    
    - 토큰이 주어졌을 때 유저 반환 테스트
    - 토큰 생성(access, refresh) 테스트
    - 토큰 유효성 검사 테스트
    
    * [test - #19] - login test
    
    - 로그인 성공 시 레디스 리프레시 토큰 테스트
    - 로그인 실패 시 예외 테스트
    
    * [fix - #19] - 불필요한 @NotNull 삭제
    
    * [fix - #19] - 불필요한 static 삭제
    
    * [remove - #19] - 중복된 파일 통합
    
    * [refactor - #19] "Bearer " 제외한 토큰 getter 작성
    
    * [test - #19] redis behavior 테스트 추가 및 리팩토링
    
    * [test - #19] 중복된 파일 통합
    
    * [test - #19] 내부 클래스 별로 tearDown 작성 및 토큰 추가 검증 테스트

--- src/main/java/com/dateplan/dateplan/domain/member/controller/AuthController.java
+++ src/main/java/com/dateplan/dateplan/domain/member/controller/AuthController.java
@@ -2,14 +2,16 @@ package com.dateplan.dateplan.domain.member.controller;
 
 import static com.dateplan.dateplan.global.constant.Auth.BEARER;
 
+import com.dateplan.dateplan.domain.member.dto.AuthToken;
 import com.dateplan.dateplan.domain.member.dto.LoginRequest;
 import com.dateplan.dateplan.domain.member.dto.PhoneAuthCodeRequest;
 import com.dateplan.dateplan.domain.member.dto.PhoneRequest;
 import com.dateplan.dateplan.domain.member.service.AuthService;
 import com.dateplan.dateplan.global.dto.response.ApiResponse;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -39,18 +41,29 @@ public class AuthController {
 	}
 
 	@PostMapping("/login")
-	public ApiResponse<Void> login(
-		@RequestBody @Valid LoginRequest loginRequest,
-		HttpServletResponse response) {
-		authService.login(loginRequest.toServiceRequest(), response);
-		return ApiResponse.ofSuccess();
+	public ResponseEntity<ApiResponse<Void>> login(@RequestBody @Valid LoginRequest loginRequest) {
+		AuthToken authToken = authService.login(loginRequest.toServiceRequest());
+		HttpHeaders responseHeaders = setHeaderTokens(authToken);
+		return ResponseEntity.ok()
+			.headers(responseHeaders)
+			.body(ApiResponse.ofSuccess());
 	}
 
 	@PostMapping("/refresh")
-	public ApiResponse<Void> refresh(
-		@RequestHeader(value = "Authorization") String refreshToken,
-		HttpServletResponse response) {
-		authService.refreshAccessToken(refreshToken.replaceAll(BEARER.getContent(), ""), response);
-		return ApiResponse.ofSuccess();
+	public ResponseEntity<ApiResponse<Void>> refresh(
+		@RequestHeader(value = "Authorization") String refreshToken) {
+		AuthToken authToken = authService.refreshAccessToken(
+			refreshToken.replaceAll(BEARER.getContent(), ""));
+		HttpHeaders responseHeaders = setHeaderTokens(authToken);
+		return ResponseEntity.ok()
+			.headers(responseHeaders)
+			.body(ApiResponse.ofSuccess());
+	}
+
+	private HttpHeaders setHeaderTokens(AuthToken authToken) {
+		HttpHeaders responseHeaders = new HttpHeaders();
+		responseHeaders.set("Authorization", authToken.getAccessToken());
+		responseHeaders.set("refreshToken", authToken.getRefreshToken());
+		return responseHeaders;
 	}
 }
--- src/main/java/com/dateplan/dateplan/domain/member/dto/AuthToken.java
+++ src/main/java/com/dateplan/dateplan/domain/member/dto/AuthToken.java
@@ -1,5 +1,6 @@
 package com.dateplan.dateplan.domain.member.dto;
 
+import static com.dateplan.dateplan.global.constant.Auth.*;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -10,4 +11,11 @@ public class AuthToken {
 	private String accessToken;
 	private String refreshToken;
 
+	public String getAccessTokenWithoutPrefix() {
+		return accessToken.replaceAll(BEARER.getContent(), "");
+	}
+
+	public String getRefreshTokenWithoutPrefix() {
+		return refreshToken.replaceAll(BEARER.getContent(), "");
+	}
 }

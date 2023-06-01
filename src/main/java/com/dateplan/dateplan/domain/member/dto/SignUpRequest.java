6eb2cd79f2f0c38dbd6537a1b654bc705cbe3154
guswls159357 <guswls159357@naver.com>
2023-06-01 13:51:13 +0900

    [Feat - #15] 회원가입 api 에서 사용할 DTO 생성

---
+++ src/main/java/com/dateplan/dateplan/domain/member/dto/SignUpRequest.java
@@ -0,0 +1,59 @@
+package com.dateplan.dateplan.domain.member.dto;
+
+import static com.dateplan.dateplan.global.constant.InputPattern.*;
+import static com.dateplan.dateplan.global.constant.InputPattern.PHONE_PATTERN;
+import static com.dateplan.dateplan.global.exception.ErrorCode.DetailMessage.*;
+
+import com.dateplan.dateplan.global.constant.Gender;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Pattern;
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignUpRequest {
+
+	@NotNull(message = INVALID_PHONE_PATTERN)
+	@Pattern(regexp = PHONE_PATTERN, message = INVALID_PHONE_PATTERN)
+	private String phone;
+
+	@NotNull(message = INVALID_MEMBER_NAME_PATTERN)
+	@Pattern(regexp = MEMBER_NAME_PATTERN, message = INVALID_MEMBER_NAME_PATTERN)
+	private String name;
+
+	@NotNull(message = INVALID_NICKNAME_PATTERN)
+	@Pattern(regexp = NICKNAME_PATTERN, message = INVALID_NICKNAME_PATTERN)
+	private String nickname;
+
+	@NotNull(message = INVALID_PASSWORD_PATTERN)
+	@Pattern(regexp = PASSWORD_PATTERN, message = INVALID_PASSWORD_PATTERN)
+	private String password;
+
+	@DateTimeFormat(iso = ISO.DATE)
+	@Past(message = INVALID_BIRTH_RANGE)
+	private LocalDate birth;
+
+	@NotNull(message = INVALID_GENDER)
+	private Gender gender;
+
+	public SignUpServiceRequest toServiceRequest() {
+
+		return SignUpServiceRequest.builder()
+			.phone(this.phone)
+			.name(this.name)
+			.nickname(this.nickname)
+			.password(this.password)
+			.birth(this.birth)
+			.gender(this.gender)
+			.build();
+	}
+}

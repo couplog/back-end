6cf6c493e698a1939dca3bb71a287f412b13c582
guswls159357 <guswls159357@naver.com>
2023-06-01 14:06:32 +0900

    [Feat - #15] 회원가입을 진행하는 메서드 작성

---
+++ src/main/java/com/dateplan/dateplan/domain/member/service/MemberService.java
@@ -0,0 +1,34 @@
+package com.dateplan.dateplan.domain.member.service;
+
+import com.dateplan.dateplan.domain.member.dto.SignUpServiceRequest;
+import com.dateplan.dateplan.domain.member.entity.Member;
+import com.dateplan.dateplan.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+
+	private final MemberRepository memberRepository;
+	private final MemberReadService memberReadService;
+	private final AuthService authService;
+
+	public void signUp(SignUpServiceRequest request) {
+
+		String phone = request.getPhone();
+		String nickname = request.getNickname();
+
+		memberReadService.throwIfPhoneExists(phone);
+		memberReadService.throwIfNicknameExists(nickname);
+
+		authService.throwIfPhoneNotAuthenticated(phone);
+
+		Member member = request.toMember();
+		memberRepository.save(member);
+
+		authService.deleteAuthenticationInfoInRedis(phone);
+	}
+}

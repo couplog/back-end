cbeb9b07ba3b4b3791b857d90f392525c6207301
kariskan <zkfzpf56@naver.com>
2023-06-03 17:10:22 +0900

    [feat - #26] 회원 연결 코드 생성 컨트롤러 작성

---
+++ src/main/java/com/dateplan/dateplan/domain/member/controller/MemberController.java
@@ -0,0 +1,25 @@
+package com.dateplan.dateplan.domain.member.controller;
+
+import com.dateplan.dateplan.domain.member.dto.ConnectionResponse;
+import com.dateplan.dateplan.domain.member.dto.ConnectionServiceResponse;
+import com.dateplan.dateplan.domain.member.service.MemberService;
+import com.dateplan.dateplan.global.dto.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/members")
+public class MemberController {
+
+	private final MemberService memberService;
+
+	@GetMapping("/connect")
+	public ApiResponse<ConnectionResponse> getConnectionCode() {
+		ConnectionServiceResponse connectionServiceResponse = memberService.readConnection();
+		return ApiResponse.ofSuccess(connectionServiceResponse.toConnectionResponse());
+	}
+
+}

91de457ceb84e404f720a5da80323a0acf0a85db
kariskan <zkfzpf56@naver.com>
2023-06-03 17:10:08 +0900

    [feat - #26] 회원 연결 코드 생성 서비스 작성

--- src/main/java/com/dateplan/dateplan/domain/member/service/MemberService.java
+++ src/main/java/com/dateplan/dateplan/domain/member/service/MemberService.java
@@ -1,9 +1,15 @@
 package com.dateplan.dateplan.domain.member.service;
 
+import com.dateplan.dateplan.domain.member.dto.ConnectionServiceResponse;
 import com.dateplan.dateplan.domain.member.dto.SignUpServiceRequest;
 import com.dateplan.dateplan.domain.member.entity.Member;
 import com.dateplan.dateplan.domain.member.repository.MemberRepository;
+import com.dateplan.dateplan.global.auth.MemberThreadLocal;
+import com.dateplan.dateplan.global.util.RandomCodeGenerator;
+import java.time.Duration;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,6 +21,8 @@ public class MemberService {
 	private final MemberRepository memberRepository;
 	private final MemberReadService memberReadService;
 	private final AuthService authService;
+	private final StringRedisTemplate redisTemplate;
+	private static final String CONNECTION_PREFIX = "[CONNECTION]";
 
 	public void signUp(SignUpServiceRequest request) {
 
@@ -30,5 +38,23 @@ public class MemberService {
 		memberRepository.save(member);
 
 		authService.deleteAuthenticationInfoInRedis(phone);
+	}
+
+	public ConnectionServiceResponse readConnection() {
+		Member member = MemberThreadLocal.get();
+
+		String key = getConnectionKey(member.getId());
+		String connectionCode = RandomCodeGenerator.generateConnectionCode(6);
+
+		ValueOperations<String, String> stringValueOperations = redisTemplate.opsForValue();
+		stringValueOperations.set(key, connectionCode);
+		stringValueOperations.getAndExpire(key, Duration.ofHours(24L));
+			return ConnectionServiceResponse.builder()
+			.connectionCode(connectionCode)
+			.build();
+	}
+
+	private String getConnectionKey(Long id) {
+		return CONNECTION_PREFIX + id;
 	}
 }

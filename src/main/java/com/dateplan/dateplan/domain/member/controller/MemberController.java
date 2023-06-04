6937c45b2cfc43294ea975ea8603103d33e08074
guswls159357 <guswls159357@naver.com>
2023-06-04 23:57:58 +0900

    [Feat - #28] presigned url 을 얻어오는 컨트롤러 메서드 작성

--- src/main/java/com/dateplan/dateplan/domain/member/controller/MemberController.java
+++ src/main/java/com/dateplan/dateplan/domain/member/controller/MemberController.java
@@ -3,7 +3,9 @@ package com.dateplan.dateplan.domain.member.controller;
 import com.dateplan.dateplan.domain.member.dto.ConnectionRequest;
 import com.dateplan.dateplan.domain.member.dto.ConnectionResponse;
 import com.dateplan.dateplan.domain.member.dto.ConnectionServiceResponse;
+import com.dateplan.dateplan.domain.member.dto.PresignedURLResponse;
 import com.dateplan.dateplan.domain.member.service.MemberService;
+import com.dateplan.dateplan.domain.s3.S3ImageType;
 import com.dateplan.dateplan.global.dto.response.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -19,6 +21,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class MemberController {
 
 	private final MemberService memberService;
+
+	@GetMapping("/profile/image/presigned-url")
+	public ApiResponse<PresignedURLResponse> getPresignedURL() {
+
+		PresignedURLResponse presingedURL = memberService.getPresingedURL(
+			S3ImageType.MEMBER_PROFILE);
+
+		return ApiResponse.ofSuccess(presingedURL);
+	}
 
 	@GetMapping("/connect")
 	public ApiResponse<ConnectionResponse> getConnectionCode() {

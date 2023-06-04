75bec8f04e76f249c8abeb8849bd5f70ff70bcb0
kariskan <zkfzpf56@naver.com>
2023-06-04 13:08:02 +0900

    [feat - #26] MemberService 작성
    - 연결된 코드 유효성 검사 후 올바른 상대 회원이 존재할 경우, Couple entity를 생성해 save

--- src/main/java/com/dateplan/dateplan/domain/member/service/MemberService.java
+++ src/main/java/com/dateplan/dateplan/domain/member/service/MemberService.java
@@ -2,12 +2,20 @@ package com.dateplan.dateplan.domain.member.service;
 
 import static com.dateplan.dateplan.global.util.RandomCodeGenerator.generateConnectionCode;
 
+import com.dateplan.dateplan.domain.couple.entity.Couple;
+import com.dateplan.dateplan.domain.couple.repository.CoupleRepository;
+import com.dateplan.dateplan.domain.couple.service.CoupleReadService;
+import com.dateplan.dateplan.domain.member.dto.ConnectionServiceRequest;
 import com.dateplan.dateplan.domain.member.dto.ConnectionServiceResponse;
 import com.dateplan.dateplan.domain.member.dto.SignUpServiceRequest;
 import com.dateplan.dateplan.domain.member.entity.Member;
 import com.dateplan.dateplan.domain.member.repository.MemberRepository;
 import com.dateplan.dateplan.global.auth.MemberThreadLocal;
+import com.dateplan.dateplan.global.exception.member.AlreadyConnectedException;
+import com.dateplan.dateplan.global.exception.member.InvalidConnectionCodeException;
+import com.dateplan.dateplan.global.exception.member.SelfConnectionNotAllowedException;
 import java.time.Duration;
+import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
@@ -19,11 +27,14 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class MemberService {
 
+	private static final String CONNECTION_PREFIX = "[CONNECTION]";
+
 	private final MemberRepository memberRepository;
 	private final MemberReadService memberReadService;
 	private final AuthService authService;
 	private final StringRedisTemplate redisTemplate;
-	private static final String CONNECTION_PREFIX = "[CONNECTION]";
+	private final CoupleRepository coupleRepository;
+	private final CoupleReadService coupleReadService;
 
 	public void signUp(SignUpServiceRequest request) {
 
@@ -64,6 +75,44 @@ public class MemberService {
 		return ConnectionServiceResponse.builder()
 			.connectionCode(connectionCode)
 			.build();
+	}
+
+	public void connectCouple(ConnectionServiceRequest request) {
+		final Member member = MemberThreadLocal.get();
+		ValueOperations<String, String> stringValueOperations = redisTemplate.opsForValue();
+		String connectionCode = request.getConnectionCode();
+		String oppositeMemberId = stringValueOperations.get(connectionCode);
+
+		throwIfInvalidConnectionCode(oppositeMemberId);
+		Member oppositeMember = memberReadService.findMemberByIdOrElseThrow(
+			Long.valueOf(oppositeMemberId));
+		throwIfAlreadyConnected(oppositeMember);
+		throwIfSelfConnection(Long.valueOf(oppositeMemberId), member.getId());
+
+		Couple couple = Couple.builder()
+			.member1(member)
+			.member2(oppositeMember)
+			.firstDate(request.getFirstDate())
+			.build();
+		coupleRepository.save(couple);
+	}
+
+	private void throwIfAlreadyConnected(Member oppositeMember) {
+		if (coupleReadService.isMemberConnected(oppositeMember)) {
+			throw new AlreadyConnectedException();
+		}
+	}
+
+	private void throwIfInvalidConnectionCode(String oppositeMemberId) {
+		if (oppositeMemberId == null) {
+			throw new InvalidConnectionCodeException();
+		}
+	}
+
+	private void throwIfSelfConnection(Long valueOf, Long id) {
+		if (Objects.equals(valueOf, id)) {
+			throw new SelfConnectionNotAllowedException();
+		}
 	}
 
 	private String getConnectionKey(Long id) {

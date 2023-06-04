2d1a19e3d4d4e6c9e22eee3482161f4cb75b7b11
kariskan <zkfzpf56@naver.com>
2023-06-04 12:12:57 +0900

    [feat - #26] 연결 코드 조회 로직 변경
    - 동일한 코드가 존재한다면 동일하지 않은 코드를 생성
    - redis 저장방식 변경 -> 동일한 코드가 존재하는지 판단하기 위해 <code, key> 형태로 저장

--- src/main/java/com/dateplan/dateplan/domain/member/service/MemberService.java
+++ src/main/java/com/dateplan/dateplan/domain/member/service/MemberService.java
@@ -1,11 +1,12 @@
 package com.dateplan.dateplan.domain.member.service;
 
+import static com.dateplan.dateplan.global.util.RandomCodeGenerator.generateConnectionCode;
+
 import com.dateplan.dateplan.domain.member.dto.ConnectionServiceResponse;
 import com.dateplan.dateplan.domain.member.dto.SignUpServiceRequest;
 import com.dateplan.dateplan.domain.member.entity.Member;
 import com.dateplan.dateplan.domain.member.repository.MemberRepository;
 import com.dateplan.dateplan.global.auth.MemberThreadLocal;
-import com.dateplan.dateplan.global.util.RandomCodeGenerator;
 import java.time.Duration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -41,17 +42,25 @@ public class MemberService {
 	}
 
 	public ConnectionServiceResponse getConnectionCode() {
-		Member member = MemberThreadLocal.get();
+		final Member member = MemberThreadLocal.get();
 		ValueOperations<String, String> stringValueOperations = redisTemplate.opsForValue();
 
 		String key = getConnectionKey(member.getId());
 		String connectionCode = stringValueOperations.get(key);
 
 		if (connectionCode == null) {
-			connectionCode = RandomCodeGenerator.generateConnectionCode(6);
+			connectionCode = generateConnectionCode(6);
+			while (stringValueOperations.get(connectionCode) != null) {
+				connectionCode = generateConnectionCode(6);
+			}
+
+			stringValueOperations.set(key, connectionCode);
+			stringValueOperations.set(connectionCode, String.valueOf(member.getId()));
+
+			stringValueOperations.getAndExpire(key, Duration.ofHours(24L));
+			stringValueOperations.getAndExpire(connectionCode, Duration.ofHours(24L));
 		}
-		stringValueOperations.set(key, connectionCode);
-		stringValueOperations.getAndExpire(key, Duration.ofHours(24L));
+
 		return ConnectionServiceResponse.builder()
 			.connectionCode(connectionCode)
 			.build();

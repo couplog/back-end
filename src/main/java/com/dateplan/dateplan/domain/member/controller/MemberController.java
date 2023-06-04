3f3a09943d55e77e94a2c1740a256d0b3fb21864
kariskan <zkfzpf56@naver.com>
2023-06-04 13:08:17 +0900

    [feat - #26] MemberController 작성

--- src/main/java/com/dateplan/dateplan/domain/member/controller/MemberController.java
+++ src/main/java/com/dateplan/dateplan/domain/member/controller/MemberController.java
@@ -1,11 +1,15 @@
 package com.dateplan.dateplan.domain.member.controller;
 
+import com.dateplan.dateplan.domain.member.dto.ConnectionRequest;
 import com.dateplan.dateplan.domain.member.dto.ConnectionResponse;
 import com.dateplan.dateplan.domain.member.dto.ConnectionServiceResponse;
 import com.dateplan.dateplan.domain.member.service.MemberService;
 import com.dateplan.dateplan.global.dto.response.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,4 +26,9 @@ public class MemberController {
 		return ApiResponse.ofSuccess(connectionServiceResponse.toConnectionResponse());
 	}
 
+	@PostMapping("/connect")
+	public ApiResponse<Void> connectCouple(@Valid @RequestBody ConnectionRequest request) {
+		memberService.connectCouple(request.toConnectionServiceRequest());
+		return ApiResponse.ofSuccess();
+	}
 }

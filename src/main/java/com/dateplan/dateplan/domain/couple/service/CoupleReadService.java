7ac7f24c82a23353e7589b8051933fc1b3cada7c
kariskan <zkfzpf56@naver.com>
2023-06-07 18:17:45 +0900

    [feat - #41] member로 couple 찾는 메서드 작성

--- src/main/java/com/dateplan/dateplan/domain/couple/service/CoupleReadService.java
+++ src/main/java/com/dateplan/dateplan/domain/couple/service/CoupleReadService.java
@@ -1,7 +1,9 @@
 package com.dateplan.dateplan.domain.couple.service;
 
+import com.dateplan.dateplan.domain.couple.entity.Couple;
 import com.dateplan.dateplan.domain.couple.repository.CoupleRepository;
 import com.dateplan.dateplan.domain.member.entity.Member;
+import com.dateplan.dateplan.global.exception.couple.CoupleNotConnectedException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +17,11 @@ public class CoupleReadService {
 
 	public boolean isMemberConnected(Member member) {
 		return coupleRepository.existsByMember1OrMember2(member, member);
+	}
+
+	public Couple findCoupleByMemberOrElseThrow(Member member) {
+		return coupleRepository.findByMember1OrMember2(member, member)
+			.orElseThrow(CoupleNotConnectedException::new);
 	}
 
 }

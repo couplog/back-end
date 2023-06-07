321754f3afcbb47de63f8c0fa63964f42c0697ea
kariskan <zkfzpf56@naver.com>
2023-06-08 01:05:32 +0900

    [feat - #41] 처음 만난 날 변경 controller 코드 작성

--- src/main/java/com/dateplan/dateplan/domain/couple/controller/CoupleController.java
+++ src/main/java/com/dateplan/dateplan/domain/couple/controller/CoupleController.java
@@ -1,10 +1,14 @@
 package com.dateplan.dateplan.domain.couple.controller;
 
+import com.dateplan.dateplan.domain.couple.dto.FirstDateRequest;
 import com.dateplan.dateplan.domain.couple.dto.FirstDateResponse;
 import com.dateplan.dateplan.domain.couple.service.CoupleService;
 import com.dateplan.dateplan.global.dto.response.ApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +23,11 @@ public class CoupleController {
 	public ApiResponse<FirstDateResponse> getFirstDate() {
 		FirstDateResponse response = coupleService.getFirstDate().toFirstDateResponse();
 		return ApiResponse.ofSuccess(response);
+	}
+
+	@PutMapping("/first-date")
+	public ApiResponse<Void> updateFirstDate(@Valid @RequestBody FirstDateRequest request) {
+		coupleService.updateFirstDate(request.toFirstDateServiceRequest());
+		return ApiResponse.ofSuccess();
 	}
 }

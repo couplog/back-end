a576488cc6cd18c02c48692fc7cb253592756811
guswls159357 <guswls159357@naver.com>
2023-06-09 00:52:37 +0900

    [Feat - #53] 현재 로그인 회원 연결 여부 조회 서비스 메서드 작성

--- src/main/java/com/dateplan/dateplan/domain/couple/service/CoupleReadService.java
+++ src/main/java/com/dateplan/dateplan/domain/couple/service/CoupleReadService.java
@@ -2,6 +2,7 @@ package com.dateplan.dateplan.domain.couple.service;
 
 import com.dateplan.dateplan.domain.couple.repository.CoupleRepository;
 import com.dateplan.dateplan.domain.member.entity.Member;
+import com.dateplan.dateplan.global.auth.MemberThreadLocal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,4 +18,10 @@ public class CoupleReadService {
 		return coupleRepository.existsByMember1OrMember2(member, member);
 	}
 
+	public boolean isCurrentLoginMemberConnected() {
+
+		Member loginMember = MemberThreadLocal.get();
+
+		return coupleRepository.existsByMember1OrMember2(loginMember, loginMember);
+	}
 }

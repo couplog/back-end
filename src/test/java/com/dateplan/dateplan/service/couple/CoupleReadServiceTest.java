70d59b149c95a89c8df137281c0f6b304b0ca4ed
kariskan <zkfzpf56@naver.com>
2023-06-10 12:43:51 +0900

    [test - #41] CoupleReadServiceTest 작성
    - 멤버가 주어졌을 때 올바른 커플을 반환하는지
    - 연결 안 된 멤버가 주어졌을 때 예외를 반환하는지

--- src/test/java/com/dateplan/dateplan/service/couple/CoupleReadServiceTest.java
+++ src/test/java/com/dateplan/dateplan/service/couple/CoupleReadServiceTest.java
@@ -1,6 +1,7 @@
 package com.dateplan.dateplan.service.couple;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.dateplan.dateplan.domain.couple.entity.Couple;
 import com.dateplan.dateplan.domain.couple.repository.CoupleRepository;
@@ -9,8 +10,11 @@ import com.dateplan.dateplan.domain.member.entity.Member;
 import com.dateplan.dateplan.domain.member.repository.MemberRepository;
 import com.dateplan.dateplan.domain.member.service.MemberReadService;
 import com.dateplan.dateplan.global.constant.Gender;
+import com.dateplan.dateplan.global.exception.ErrorCode.DetailMessage;
+import com.dateplan.dateplan.global.exception.couple.CoupleNotConnectedException;
 import com.dateplan.dateplan.service.ServiceTestSupport;
 import java.time.LocalDate;
+import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -47,8 +51,8 @@ public class CoupleReadServiceTest extends ServiceTestSupport {
 
 		@BeforeEach
 		void setUp() {
-			Member member1 = createMember(phone1, "password");
-			Member member2 = createMember(phone2, "password");
+			Member member1 = createMember(phone1);
+			Member member2 = createMember(phone2);
 			memberRepository.save(member1);
 			memberRepository.save(member2);
 		}
@@ -88,13 +92,63 @@ public class CoupleReadServiceTest extends ServiceTestSupport {
 		}
 	}
 
-	private Member createMember(String phone, String password) {
+	@Nested
+	@DisplayName("멤버로 연결되어 있는 커플을 찾을 때")
+	class FindCoupleByMember {
+
+		Member connectedMember1;
+		Member connectedMember2;
+		Member notConnectedMember;
+
+		@BeforeEach
+		void setUp() {
+			connectedMember1 = createMember("01012345678");
+			connectedMember2 = createMember("01012345679");
+			notConnectedMember = createMember("01012345670");
+			memberRepository.saveAll(
+				List.of(connectedMember1, connectedMember2, notConnectedMember));
+			Couple couple = Couple.builder()
+				.member1(connectedMember1)
+				.member2(connectedMember2)
+				.firstDate(LocalDate.of(2010, 10, 10))
+				.build();
+			coupleRepository.save(couple);
+		}
+
+		@AfterEach
+		void tearDown() {
+			coupleRepository.deleteAllInBatch();
+			memberRepository.deleteAllInBatch();
+		}
+
+		@DisplayName("커플이 연결되어 있는 멤버를 입력하면 올바른 커플을 반환한다")
+		@Test
+		void returnCoupleWithConnectedMember() {
+
+			Couple couple1 = coupleReadService.findCoupleByMemberOrElseThrow(connectedMember1);
+			Couple couple2 = coupleReadService.findCoupleByMemberOrElseThrow(connectedMember2);
+
+			assertThat(couple1.getId()).isEqualTo(couple2.getId());
+		}
+
+		@DisplayName("커플이 연결되어 있지 않은 멤버를 입력하면 실패한다")
+		@Test
+		void failWithNotConnectedMember() {
+
+			assertThatThrownBy(
+				() -> coupleReadService.findCoupleByMemberOrElseThrow(notConnectedMember))
+				.isInstanceOf(CoupleNotConnectedException.class)
+				.hasMessage(DetailMessage.COUPLE_NOT_CONNECTED);
+		}
+	}
+
+	private Member createMember(String phone) {
 
 		return Member.builder()
 			.name("name")
 			.nickname("nickname")
 			.phone(phone)
-			.password(password)
+			.password("password")
 			.gender(Gender.FEMALE)
 			.build();
 	}

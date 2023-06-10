3f4bf8c2aba5449ebbcc240245484b2a871de370
kariskan <zkfzpf56@naver.com>
2023-06-10 12:32:32 +0900

    [test - #41] 커플 처음 만난 날 test 작성
    - 커플 처음 만난 날 조회 시
    - 커플 처음 만난 날 수정 시

--- src/test/java/com/dateplan/dateplan/controller/couple/CoupleControllerTest.java
+++ src/test/java/com/dateplan/dateplan/controller/couple/CoupleControllerTest.java
@@ -3,11 +3,16 @@ package com.dateplan.dateplan.controller.couple;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
+import static org.mockito.BDDMockito.willThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.dateplan.dateplan.controller.ControllerTestSupport;
+import com.dateplan.dateplan.domain.couple.dto.FirstDateRequest;
+import com.dateplan.dateplan.domain.couple.dto.FirstDateServiceRequest;
 import com.dateplan.dateplan.domain.couple.dto.FirstDateServiceResponse;
 import com.dateplan.dateplan.global.constant.Operation;
 import com.dateplan.dateplan.global.constant.Resource;
@@ -17,11 +22,13 @@ import com.dateplan.dateplan.global.exception.NoPermissionException;
 import com.dateplan.dateplan.global.exception.couple.CoupleNotConnectedException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
 
 public class CoupleControllerTest extends ControllerTestSupport {
 
@@ -113,8 +120,108 @@ public class CoupleControllerTest extends ControllerTestSupport {
 		}
 	}
 
+	@Nested
+	@DisplayName("커플 처음 만난 날 수정 시")
+	class UpdateFirstDate {
+
+		private static final String REQUEST_URL = "/api/couples/{couple_id}/first-date";
+
+		@DisplayName("올바른 coupleId, request를 요청하면 성공한다")
+		@Test
+		void successWithValidCoupleIdAndRequest() throws Exception {
+
+			// Given
+			FirstDateRequest request = createFirstDateRequest();
+
+			// Stub
+			willDoNothing()
+				.given(coupleService)
+				.updateFirstDate(anyLong(), any(FirstDateServiceRequest.class));
+
+			// When & Then
+			mockMvc.perform(
+					put(REQUEST_URL, 1L)
+						.content(om.writeValueAsString(request))
+						.contentType(MediaType.APPLICATION_JSON)
+						.characterEncoding(StandardCharsets.UTF_8))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.success").value("true"));
+		}
+
+		@DisplayName("Long 타입이 아닌 couple-id를 적으면 실패한다.")
+		@Test
+		void failWithInvalidType() throws Exception {
+
+			FirstDateRequest request = createFirstDateRequest();
+
+			// When & Then
+			mockMvc.perform(
+					put(REQUEST_URL, "a")
+						.content(om.writeValueAsString(request))
+						.contentType(MediaType.APPLICATION_JSON)
+						.characterEncoding(StandardCharsets.UTF_8))
+				.andExpect(status().isBadRequest())
+				.andExpectAll(
+					jsonPath("$.success").value("false"),
+					jsonPath("$.code").value(ErrorCode.METHOD_ARGUMENT_TYPE_MISMATCH.getCode()));
+		}
+
+		@DisplayName("현재 자신이 커플에 연결되어 있지 않으면 실패한다")
+		@Test
+		void failWithNotConnected() throws Exception {
+
+			// Given
+			FirstDateRequest request = createFirstDateRequest();
+
+			// Stub
+			willThrow(new CoupleNotConnectedException())
+				.given(coupleService)
+				.updateFirstDate(anyLong(), any(FirstDateServiceRequest.class));
+
+			// When & Then
+			mockMvc.perform(
+					put(REQUEST_URL, 1L)
+						.content(om.writeValueAsString(request))
+						.contentType(MediaType.APPLICATION_JSON)
+						.characterEncoding(StandardCharsets.UTF_8))
+				.andExpect(status().isBadRequest())
+				.andExpectAll(
+					jsonPath("$.success").value("false"),
+					jsonPath("$.code").value(ErrorCode.COUPLE_NOT_CONNECTED.getCode()),
+					jsonPath("$.message").value(DetailMessage.COUPLE_NOT_CONNECTED));
+		}
+
+		@DisplayName("현재보다 이후의 시간을 처음 만난 날로 수정하려하면 실패한다")
+		@Test
+		void failWithNowPastFirstDate() throws Exception {
+
+			// Given
+			FirstDateRequest request = FirstDateRequest.builder()
+				.firstDate(LocalDate.now().plusDays(1L))
+				.build();
+
+			// When & Then
+			mockMvc.perform(
+					put(REQUEST_URL, 1L)
+						.content(om.writeValueAsString(request))
+						.contentType(MediaType.APPLICATION_JSON)
+						.characterEncoding(StandardCharsets.UTF_8))
+				.andExpect(status().isBadRequest())
+				.andExpectAll(
+					jsonPath("$.success").value("false"),
+					jsonPath("$.code").value(ErrorCode.INVALID_INPUT_VALUE.getCode()),
+					jsonPath("$.message").value(DetailMessage.INVALID_FIRST_DATE_RANGE));
+		}
+	}
+
 	private FirstDateServiceResponse createFirstDateResponse() {
 		return FirstDateServiceResponse.builder()
+			.firstDate(LocalDate.of(2010, 10, 10))
+			.build();
+	}
+
+	private FirstDateRequest createFirstDateRequest() {
+		return FirstDateRequest.builder()
 			.firstDate(LocalDate.of(2010, 10, 10))
 			.build();
 	}

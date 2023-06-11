0f1c17a5d3d6fb31577d1ffd51d45cbb554a38b7
kariskan <zkfzpf56@naver.com>
2023-06-11 22:51:59 +0900

    [refactor - #52] 파트너 아이디 구하는 로직 entity 내로 이동

--- src/main/java/com/dateplan/dateplan/domain/couple/service/CoupleReadService.java
+++ src/main/java/com/dateplan/dateplan/domain/couple/service/CoupleReadService.java
@@ -6,7 +6,6 @@ import com.dateplan.dateplan.domain.couple.repository.CoupleRepository;
 import com.dateplan.dateplan.domain.member.entity.Member;
 import com.dateplan.dateplan.global.exception.couple.MemberNotConnectedException;
 import com.dateplan.dateplan.global.auth.MemberThreadLocal;
-import java.util.Objects;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -34,26 +33,16 @@ public class CoupleReadService {
 		return coupleRepository.existsByMember1OrMember2(loginMember, loginMember);
 	}
 
-
-
-	@Transactional(readOnly = true)
 	public CoupleInfoServiceResponse getCoupleInfo() {
 		final Member member = MemberThreadLocal.get();
 
 		Couple couple = findCoupleByMemberOrElseThrow(member);
-		Long partnerId = getPartnerId(couple, member);
+		Long partnerId = couple.getPartnerId(member);
 
 		return CoupleInfoServiceResponse.builder()
 			.coupleId(couple.getId())
 			.partnerId(partnerId)
 			.firstDate(couple.getFirstDate())
 			.build();
-	}
-
-	private Long getPartnerId(Couple couple, Member member) {
-		if (Objects.equals(couple.getMember1().getId(), member.getId())) {
-			return couple.getMember2().getId();
-		}
-		return couple.getMember1().getId();
 	}
 }

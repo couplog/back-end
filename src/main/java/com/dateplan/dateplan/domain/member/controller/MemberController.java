a296e84a900ad5b4b5f352e1d05605d23a3185c6
kariskan <zkfzpf56@naver.com>
2023-06-13 18:00:36 +0900

    [feat - #63] 일정 추가를 위한 controller 작성

--- src/main/java/com/dateplan/dateplan/domain/member/controller/MemberController.java
+++ src/main/java/com/dateplan/dateplan/domain/member/controller/MemberController.java
@@ -10,6 +10,8 @@ import com.dateplan.dateplan.domain.member.dto.MemberInfoServiceResponse;
 import com.dateplan.dateplan.domain.member.dto.PresignedURLResponse;
 import com.dateplan.dateplan.domain.member.service.MemberReadService;
 import com.dateplan.dateplan.domain.member.service.MemberService;
+import com.dateplan.dateplan.domain.schedule.dto.ScheduleRequest;
+import com.dateplan.dateplan.domain.schedule.service.ScheduleService;
 import com.dateplan.dateplan.global.dto.response.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -31,6 +33,7 @@ public class MemberController {
 	private final MemberReadService memberReadService;
 	private final CoupleService coupleService;
 	private final CoupleReadService coupleReadService;
+	private final ScheduleService scheduleService;
 
 	@GetMapping("/me")
 	public ApiResponse<MemberInfoResponse> getCurrentLoginMemberInfo(){
@@ -78,6 +81,13 @@ public class MemberController {
 	public ApiResponse<Void> connectCouple(@PathVariable("member_id") Long memberId,
 		@Valid @RequestBody ConnectionRequest request) {
 		coupleService.connectCouple(memberId, request.toConnectionServiceRequest());
+		return ApiResponse.ofSuccess();
+	}
+
+	@PostMapping("/{member_id}/schedules")
+	public ApiResponse<Void> createSchedule(@PathVariable("member_id") Long memberId,
+		@Valid @RequestBody ScheduleRequest request) {
+		scheduleService.createSchedule(memberId, request.toScheduleServiceRequest());
 		return ApiResponse.ofSuccess();
 	}
 }

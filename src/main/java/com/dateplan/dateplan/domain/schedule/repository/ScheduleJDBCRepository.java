fd26859a515d7f60e601569417c6d956282505f7
kariskan <zkfzpf56@naver.com>
2023-06-15 01:01:22 +0900

    [feat - #63] @Transactional 어노테이션 추가

--- src/main/java/com/dateplan/dateplan/domain/schedule/repository/ScheduleJDBCRepository.java
+++ src/main/java/com/dateplan/dateplan/domain/schedule/repository/ScheduleJDBCRepository.java
@@ -9,9 +9,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.BatchPreparedStatementSetter;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 @RequiredArgsConstructor
+@Transactional
 public class ScheduleJDBCRepository {
 
 	private final ScheduleRepository scheduleRepository;

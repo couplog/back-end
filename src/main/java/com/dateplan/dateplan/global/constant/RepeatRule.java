c987b90e5f8d7aa5abe0552805f8db5a4920374d
kariskan <zkfzpf56@naver.com>
2023-06-14 16:18:26 +0900

    [feat - #63] RepeatRule enum 수정

--- src/main/java/com/dateplan/dateplan/global/constant/RepeatRule.java
+++ src/main/java/com/dateplan/dateplan/global/constant/RepeatRule.java
@@ -4,21 +4,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 
 public enum RepeatRule {
 
-	N("없음"),
-	D("매일"),
-	W("매주"),
-	M("매월"),
-	Y("매년");
-
-	private final String rule;
-
-	RepeatRule(String rule) {
-		this.rule = rule;
-	}
-
-	public String getRule() {
-		return rule;
-	}
+	N, D, W, M, Y;
 
 	@JsonCreator
 	public static RepeatRule from(String rule) {

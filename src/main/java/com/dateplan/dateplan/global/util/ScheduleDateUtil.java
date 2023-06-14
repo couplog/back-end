c26764e34cd0f151317baac90f6db0484cc29a94
kariskan <zkfzpf56@naver.com>
2023-06-14 16:23:21 +0900

    [feat - #63] ScheduleDateUtil 수정
    - getNextCycle(), 다음 주기를 구하는 메서드 작성

--- src/main/java/com/dateplan/dateplan/global/util/ScheduleDateUtil.java
+++ src/main/java/com/dateplan/dateplan/global/util/ScheduleDateUtil.java
@@ -1,34 +1,56 @@
 package com.dateplan.dateplan.global.util;
 
+import com.dateplan.dateplan.global.constant.RepeatRule;
 import java.time.DateTimeException;
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ScheduleDateUtil {
 
-	public static LocalDate getNextMonthDate(int year, int month, int day) {
+	public static LocalDateTime getNextCycle(LocalDateTime now, RepeatRule rule) {
+		if (rule.equals(RepeatRule.D)) {
+			return getNextDayDate(now);
+		}
+		if (rule.equals(RepeatRule.W)) {
+			return getNextWeekDate(now);
+		}
+		if (rule.equals(RepeatRule.M)) {
+			return getNextMonthDate(now.getYear(), now.getMonthValue(), now.getDayOfMonth(), now);
+		}
+		return getNextYearDate(now.getYear(), now.getMonthValue(), now.getDayOfMonth(), now);
+	}
+
+	private static LocalDateTime getNextDayDate(LocalDateTime now) {
+		return now.plusDays(1);
+	}
+
+	public static LocalDateTime getNextWeekDate(LocalDateTime now) {
+		return now.plusDays(7);
+	}
+
+	private static LocalDateTime getNextMonthDate(int year, int month, int day, LocalDateTime now) {
 		do {
 			month++;
 			if (month > 12) {
 				year++;
 				month = 1;
 			}
-		} while(!isValidDate(year, month, day));
-		return LocalDate.of(year, month, day);
+		} while (!isValidDate(year, month, day, now));
+		return LocalDateTime.of(year, month, day, now.getHour(), now.getMinute());
 	}
 
-	public static LocalDate getNextYearDate(int year, int month, int day) {
+	private static LocalDateTime getNextYearDate(int year, int month, int day, LocalDateTime now) {
 		do {
 			year++;
-		} while(!isValidDate(year, month, day));
-		return LocalDate.of(year, month, day);
+		} while (!isValidDate(year, month, day, now));
+		return LocalDateTime.of(year, month, day, now.getHour(), now.getMinute());
 	}
 
-	private static boolean isValidDate(int year, int month, int day) {
+	private static boolean isValidDate(int year, int month, int day, LocalDateTime now) {
 		try {
-			LocalDate.of(year, month, day);
+			LocalDateTime.of(year, month, day, now.getHour(), now.getMinute());
 			return true;
 		} catch (DateTimeException e) {
 			return false;

c7d0a0c4f149fb05812242c82974db5a6442e950
guswls159357 <guswls159357@naver.com>
2023-06-17 17:32:33 +0900

    [Feat - #67] 기념일 생성시 필요한 DTO 작성

---
+++ src/main/java/com/dateplan/dateplan/domain/anniversary/dto/AnniversaryCreateServiceRequest.java
@@ -0,0 +1,42 @@
+package com.dateplan.dateplan.domain.anniversary.dto;
+
+import static com.dateplan.dateplan.domain.anniversary.entity.AnniversaryRepeatRule.NONE;
+import static com.dateplan.dateplan.global.constant.DateConstants.CALENDER_END_DATE;
+
+import com.dateplan.dateplan.domain.anniversary.entity.AnniversaryPattern;
+import com.dateplan.dateplan.domain.anniversary.entity.AnniversaryRepeatRule;
+import com.dateplan.dateplan.domain.couple.entity.Couple;
+import java.time.LocalDate;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class AnniversaryCreateServiceRequest {
+
+	private String title;
+	private String content;
+	private AnniversaryRepeatRule repeatRule;
+	private LocalDate date;
+
+	@Builder
+	public AnniversaryCreateServiceRequest(String title, String content,
+		AnniversaryRepeatRule repeatRule, LocalDate date) {
+		this.title = title;
+		this.content = content;
+		this.repeatRule = repeatRule;
+		this.date = date;
+	}
+
+	public AnniversaryPattern toAnniversaryPattern(Couple couple) {
+
+		LocalDate repeatEndDate = repeatRule == NONE ? date : CALENDER_END_DATE;
+
+		return AnniversaryPattern.builder()
+			.couple(couple)
+			.repeatStartDate(date)
+			.repeatEndDate(repeatEndDate)
+			.repeatRule(repeatRule)
+			.build();
+	}
+
+}

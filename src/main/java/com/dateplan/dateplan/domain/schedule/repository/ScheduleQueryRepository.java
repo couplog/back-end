21bcb30f0d14470deba53b2afd5c86c7732e616a
kariskan <zkfzpf56@naver.com>
2023-06-19 23:12:55 +0900

    [feat - #75] querydsl을 이용한 개인 일정 조회 메서드 작성

---
+++ src/main/java/com/dateplan/dateplan/domain/schedule/repository/ScheduleQueryRepository.java
@@ -0,0 +1,49 @@
+package com.dateplan.dateplan.domain.schedule.repository;
+
+import static com.dateplan.dateplan.domain.member.entity.QMember.member;
+import static com.dateplan.dateplan.domain.schedule.entity.QSchedule.schedule;
+import static com.dateplan.dateplan.domain.schedule.entity.QSchedulePattern.schedulePattern;
+
+import com.dateplan.dateplan.domain.schedule.entity.Schedule;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor
+@Repository
+public class ScheduleQueryRepository {
+
+	private final JPAQueryFactory queryFactory;
+
+	public List<Schedule> findByYearAndMonth(Long memberId, Integer year, Integer month) {
+		return queryFactory
+			.select(schedule)
+			.from(schedule)
+			.join(schedule.schedulePattern, schedulePattern)
+			.join(schedulePattern.member, member)
+			.where(memberIdEq(memberId)
+				.and(yearEq(year))
+				.and(monthEq(month)))
+			.fetch();
+	}
+
+	private BooleanExpression memberIdEq(Long memberId) {
+		return schedulePattern.member.id.eq(memberId);
+	}
+
+	private BooleanExpression yearEq(Integer year) {
+		if (year == null) {
+			return null;
+		}
+		return schedule.startDateTime.year().eq(year);
+	}
+
+	private BooleanExpression monthEq(Integer month) {
+		if (month == null) {
+			return null;
+		}
+		return schedule.startDateTime.month().eq(month);
+	}
+}

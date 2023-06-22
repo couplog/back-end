7794943e5c0b087ec58b6aafd0c72d853134bb30
guswls159357 <guswls159357@naver.com>
2023-06-22 15:50:24 +0900

    [FEAT - #77] 기념일 날짜 조회 서비스 메서드 작성

---
+++ src/main/java/com/dateplan/dateplan/domain/anniversary/service/AnniversaryReadService.java
@@ -0,0 +1,41 @@
+package com.dateplan.dateplan.domain.anniversary.service;
+
+import com.dateplan.dateplan.domain.anniversary.dto.AnniversaryDatesServiceResponse;
+import com.dateplan.dateplan.domain.anniversary.entity.Anniversary;
+import com.dateplan.dateplan.domain.anniversary.repository.AnniversaryQueryRepository;
+import com.dateplan.dateplan.domain.couple.service.CoupleReadService;
+import com.dateplan.dateplan.domain.member.entity.Member;
+import com.dateplan.dateplan.global.constant.Operation;
+import com.dateplan.dateplan.global.constant.Resource;
+import com.dateplan.dateplan.global.exception.auth.NoPermissionException;
+import java.util.List;
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+@Transactional(readOnly = true)
+public class AnniversaryReadService {
+
+	private final CoupleReadService coupleReadService;
+
+	private final AnniversaryQueryRepository anniversaryQueryRepository;
+
+	public AnniversaryDatesServiceResponse readAnniversaryDates(Member loginMember,
+		Long targetCoupleId, Integer year, Integer month) {
+
+		Long loginMemberCoupleId = coupleReadService.findCoupleByMemberOrElseThrow(
+			loginMember).getId();
+
+		if (!Objects.equals(loginMemberCoupleId, targetCoupleId)) {
+			throw new NoPermissionException(Resource.COUPLE, Operation.READ);
+		}
+
+		List<Anniversary> anniversaries = anniversaryQueryRepository.findAllByCoupleIdAndYearAndMonth(
+			targetCoupleId, year, month);
+
+		return AnniversaryDatesServiceResponse.from(anniversaries);
+	}
+}

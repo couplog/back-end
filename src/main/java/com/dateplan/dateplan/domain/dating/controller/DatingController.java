4af552a849a4605966272c6ea599b266aaec9816
kariskan <zkfzpf56@naver.com>
2023-07-05 16:07:31 +0900

    [feat - #103] 데이트 일정 날짜를 조회하는 controller 작성

--- src/main/java/com/dateplan/dateplan/domain/dating/controller/DatingController.java
+++ src/main/java/com/dateplan/dateplan/domain/dating/controller/DatingController.java
@@ -1,17 +1,22 @@
 package com.dateplan.dateplan.domain.dating.controller;
 
 import com.dateplan.dateplan.domain.dating.controller.dto.request.DatingCreateRequest;
+import com.dateplan.dateplan.domain.dating.controller.dto.response.DatingDatesResponse;
+import com.dateplan.dateplan.domain.dating.service.DatingReadService;
 import com.dateplan.dateplan.domain.dating.service.DatingService;
+import com.dateplan.dateplan.domain.dating.service.dto.response.DatingDatesServiceResponse;
 import com.dateplan.dateplan.domain.member.entity.Member;
 import com.dateplan.dateplan.global.auth.MemberThreadLocal;
 import com.dateplan.dateplan.global.dto.response.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DatingController {
 
 	private final DatingService datingService;
+	private final DatingReadService datingReadService;
 
 	@ResponseStatus(value = HttpStatus.CREATED)
 	@PostMapping("/{couple_id}/dating")
@@ -31,6 +37,18 @@ public class DatingController {
 		final Member member = MemberThreadLocal.get();
 		datingService.createDating(member, coupleId, request.toDatingCreateServiceRequest());
 		return ApiResponse.ofSuccess();
+	}
+
+	@GetMapping("/{couple_id}/dating/dates")
+	public ApiResponse<DatingDatesResponse> readDatingDates(
+		@PathVariable("couple_id") Long coupleId,
+		@RequestParam(value = "year", required = false) Integer year,
+		@RequestParam(value = "month", required = false) Integer month
+	) {
+		final Member member = MemberThreadLocal.get();
+		DatingDatesServiceResponse response = datingReadService.readDatingDates(
+			member, coupleId, year, month);
+		return ApiResponse.ofSuccess(DatingDatesResponse.from(response));
 	}
 
 }

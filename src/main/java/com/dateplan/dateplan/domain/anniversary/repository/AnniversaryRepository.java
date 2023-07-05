81a04ce8623b1d97e2668a763890663f174ea8e2
guswls159357 <guswls159357@naver.com>
2023-07-05 15:38:06 +0900

    [FEAT - #96] 기념일 패턴 id 로 관련된 모든 기념일을 삭제하는 쿼리 메소드 작성

--- src/main/java/com/dateplan/dateplan/domain/anniversary/repository/AnniversaryRepository.java
+++ src/main/java/com/dateplan/dateplan/domain/anniversary/repository/AnniversaryRepository.java
@@ -2,7 +2,13 @@ package com.dateplan.dateplan.domain.anniversary.repository;
 
 import com.dateplan.dateplan.domain.anniversary.entity.Anniversary;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface AnniversaryRepository extends JpaRepository<Anniversary, Long> {
 
+	@Modifying
+	@Query("DELETE FROM Anniversary a WHERE a.anniversaryPattern.id = :anniversaryPatternId")
+	void deleteAllByAnniversaryPatternId(@Param("anniversaryPatternId") Long anniversaryPatternId);
 }

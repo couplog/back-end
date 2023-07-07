7db88d899f61556222558375fa569bdcaec57cea
kariskan <zkfzpf56@naver.com>
2023-07-07 18:31:57 +0900

    [feat - #115] 데이트 일정 수정하는 controller 작성

--- src/main/java/com/dateplan/dateplan/domain/dating/controller/DatingController.java
+++ src/main/java/com/dateplan/dateplan/domain/dating/controller/DatingController.java
@@ -1,6 +1,7 @@
 package com.dateplan.dateplan.domain.dating.controller;
 
 import com.dateplan.dateplan.domain.dating.controller.dto.request.DatingCreateRequest;
+import com.dateplan.dateplan.domain.dating.controller.dto.request.DatingUpdateRequest;
 import com.dateplan.dateplan.domain.dating.controller.dto.response.DatingDatesResponse;
 import com.dateplan.dateplan.domain.dating.service.DatingReadService;
 import com.dateplan.dateplan.domain.dating.service.DatingService;
@@ -14,6 +15,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -51,4 +53,15 @@ public class DatingController {
 		return ApiResponse.ofSuccess(DatingDatesResponse.from(response));
 	}
 
+	@PutMapping("/{couple_id}/dating/{dating_id}")
+	public ApiResponse<Void> updateDating(
+		@PathVariable("couple_id") Long coupleId,
+		@PathVariable("dating_id") Long datingId,
+		@Valid @RequestBody DatingUpdateRequest request
+	) {
+		final Member member = MemberThreadLocal.get();
+		datingService.updateDating(member, coupleId, datingId,
+			request.toDatingUpdateServiceRequest());
+		return ApiResponse.ofSuccess();
+	}
 }

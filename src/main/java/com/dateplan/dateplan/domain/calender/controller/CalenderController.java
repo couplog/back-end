462f84d49e0431857215a4c530c1e39ba9f5bf0e
kariskan <zkfzpf56@naver.com>
2023-07-13 17:12:09 +0900

    [feat - #127] 달력 일정 조회 controller 작성

---
+++ src/main/java/com/dateplan/dateplan/domain/calender/controller/CalenderController.java
@@ -0,0 +1,35 @@
+package com.dateplan.dateplan.domain.calender.controller;
+
+import com.dateplan.dateplan.domain.calender.controller.dto.response.CalenderDateResponse;
+import com.dateplan.dateplan.domain.calender.service.CalenderReadService;
+import com.dateplan.dateplan.domain.calender.service.dto.response.CalenderDateServiceResponse;
+import com.dateplan.dateplan.domain.member.entity.Member;
+import com.dateplan.dateplan.global.auth.MemberThreadLocal;
+import com.dateplan.dateplan.global.dto.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/members/{member_id}")
+public class CalenderController {
+
+	private final CalenderReadService calenderReadService;
+
+	@GetMapping("/calender/date")
+	public ApiResponse<CalenderDateResponse> readCalenderDate(
+		@PathVariable(value = "member_id") Long memberId,
+		@RequestParam(value = "coupleId") Long coupleId,
+		@RequestParam(value = "year") Integer year,
+		@RequestParam(value = "month") Integer month
+	) {
+		final Member member = MemberThreadLocal.get();
+		CalenderDateServiceResponse response = calenderReadService.readCalenderDates(
+			member, memberId, coupleId, year, month);
+		return ApiResponse.ofSuccess(CalenderDateResponse.from(response));
+	}
+}

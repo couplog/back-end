dfefcc67d8c5c646984e830ae1096a070a75e409
kariskan <zkfzpf56@naver.com>
2023-07-18 17:06:30 +0900

    [fix - #137] @NotNull Annotation 제거

--- src/main/java/com/dateplan/dateplan/domain/member/controller/dto/request/CheckPasswordRequest.java
+++ src/main/java/com/dateplan/dateplan/domain/member/controller/dto/request/CheckPasswordRequest.java
@@ -1,9 +1,6 @@
 package com.dateplan.dateplan.domain.member.controller.dto.request;
 
-import static com.dateplan.dateplan.global.exception.ErrorCode.DetailMessage.INVALID_PASSWORD_PATTERN;
-
 import com.dateplan.dateplan.domain.member.service.dto.request.CheckPasswordServiceRequest;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,7 +12,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class CheckPasswordRequest {
 
-	@NotNull(message = INVALID_PASSWORD_PATTERN)
 	private String password;
 
 	public CheckPasswordServiceRequest toCheckPasswordServiceRequest() {

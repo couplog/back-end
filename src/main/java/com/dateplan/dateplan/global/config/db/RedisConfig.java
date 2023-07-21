b9ae4d7cecc97babe98d7ff2c7a5035a981db94a
guswls159357 <guswls159357@naver.com>
2023-07-21 14:51:19 +0900

    test

--- src/main/java/com/dateplan/dateplan/global/config/db/RedisConfig.java
+++ src/main/java/com/dateplan/dateplan/global/config/db/RedisConfig.java
@@ -3,11 +3,13 @@ package com.dateplan.dateplan.global.config.db;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
+@Profile("!test")
 public class RedisConfig {
 
 	@Value("${spring.redis.host}")

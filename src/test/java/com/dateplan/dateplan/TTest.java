da179a3190e1c5e2d28c0d125082c64a231074a5
guswls159357 <guswls159357@naver.com>
2023-07-24 20:39:40 +0900

    test

---
+++ src/test/java/com/dateplan/dateplan/TTest.java
@@ -0,0 +1,15 @@
+package com.dateplan.dateplan;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class TTest {
+
+	@Test
+	void t(){
+
+		assertThat(1).isEqualTo(0);
+	}
+
+}
